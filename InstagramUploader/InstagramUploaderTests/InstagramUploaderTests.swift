2cbe7eadd6e58ded92bed58041c31bfd1dbaffd3
Yunsu Guk <krgoodnews@gmail.com>
2020-04-20 23:58:08 +0900

    Test: ViewModel -> RatioButton Test

--- InstagramUploader/InstagramUploaderTests/InstagramUploaderTests.swift
+++ InstagramUploader/InstagramUploaderTests/InstagramUploaderTests.swift
@@ -7,7 +7,39 @@
 //
 
 import XCTest
+import RxTest
+@testable import 스샷그램
 
 class InstagramUploaderTests: XCTestCase {
+
+    func test_뷰모델_생성시_Ratio_초기값은_Portrait() {
+        let viewModel = ProcessImageViewModel()
+        XCTAssertEqual(viewModel.bindableImageRatio.value, .portrait)
+    }
+
+    func test_Ratio버튼_한번_누르면_landscape() {
+        let viewModel = ProcessImageViewModel()
+        viewModel.ratioTapEvent.accept(())
+        XCTAssertEqual(viewModel.bindableImageRatio.value, .landscape)
+    }
+
+    func test_Ratio버튼_두번_누르면_square() {
+        let viewModel = ProcessImageViewModel()
+
+        viewModel.ratioTapEvent.accept(())
+        viewModel.ratioTapEvent.accept(())
+
+        XCTAssertEqual(viewModel.bindableImageRatio.value, .square)
+    }
+
+    func test_Ratio버튼_세번_누르면_다시_portrait() {
+        let viewModel = ProcessImageViewModel()
+
+        viewModel.ratioTapEvent.accept(())
+        viewModel.ratioTapEvent.accept(())
+        viewModel.ratioTapEvent.accept(())
+
+        XCTAssertEqual(viewModel.bindableImageRatio.value, .portrait)
+    }
 
 }
